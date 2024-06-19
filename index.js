6779db1f01765d5f2f688477996c12067e030f8f
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-06-20 01:19:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const {
+    renderToReadableStream,
+} = require("react-server-dom-webpack/server.edge");
+const { cache, createElement } = require("react");
+
+const getId = cache(() => ({ id: Math.random() }));
+
+const A = async () => {
+    await new Promise((res) => setTimeout(res, 10));
+    const id = getId();
+    console.log(id);
+    return null;
+};
+
+const B = async () => {
+    const id = getId();
+    console.log(id);
+    return createElement(A, {}, null);
+};
+
+renderToReadableStream(createElement(B, {}, null), null);
