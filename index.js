3124de038eb8275c742b037d8f0fd180d7d998f3
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-06-20 08:53:34 +0000

    fix: shorter repro code

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const {
-    renderToReadableStream,
+  renderToReadableStream,
 } = require("react-server-dom-webpack/server.edge");
 const { cache, createElement } = require("react");
 
 const getId = cache(() => ({ id: Math.random() }));
 
 const A = async () => {
-    await new Promise((res) => setImmediate(res));
-    const id = getId();
-    console.log(id);
-    return null;
+  await new Promise(setImmediate);
+  const id = getId();
+  console.log(id);
+  return null;
 };
 
 const B = async () => {
-    const id = getId();
-    console.log(id);
-    return createElement(A, {}, null);
+  const id = getId();
+  console.log(id);
+  return createElement(A, {}, null);
 };
 
 renderToReadableStream(createElement(B, {}, null), null);
